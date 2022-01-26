8a051b6a056e90b91c8b8b255ea22b15e74670d2
Matthew Miner <matthew.miner@unity3d.com>
2022-01-25 21:40:48 -0800

    Add dummy script for testing

---
+++ Assets/Scripts/ChuckTesta.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Dummy script for testing stuff. Will be stripped out in builds.
+/// </summary>
+class ChuckTesta : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    void Start()
+    {
+        // Put whatever you want here, just don't commit your changes.
+    }
+
+#endif
+}
