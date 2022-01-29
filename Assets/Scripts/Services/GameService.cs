061925ccd46a62c9160542bbae266a99f73ac41d
Matthew Miner <matthew.miner@unity3d.com>
2022-01-28 18:11:15 -0800

    Randomize random number generator on start

--- Assets/Scripts/Services/GameService.cs
+++ Assets/Scripts/Services/GameService.cs
@@ -1,11 +1,20 @@
-using JetBrains.Annotations;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 class GameService : Services.Service
 {
     [SerializeField] GameObject playerPrefab;
 
     bool isPlayer1 = true;
+
+    void Awake()
+    {
+        // If we don't reset the random number generator, the game uses the seed from the previous run.
+        // The random number generator state persists between game sessions.
+        // We don't want this to happen, so randomize it now.
+        Random.InitState((int)DateTime.Now.Ticks);
+    }
 
     public void StartGame(string code = null)
     {
@@ -16,6 +25,7 @@ class GameService : Services.Service
         else
         {
             code = GameCodeUtility.GenerateGameCode();
+            Debug.Log($"Generated code: {code}");
         }
 
         Services.Get<UIService>().ShowGameCode(code);
