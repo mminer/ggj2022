fd4622dc36641ff11e275c50679ae19ae7ad4685
Brad Keys <brad@bradkeys.com>
2022-01-28 20:22:29 -0500

    Create title and join screens. #3

--- Assets/Scripts/Services/UIService.cs
+++ Assets/Scripts/Services/UIService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -10,9 +11,40 @@ class UIService : Services.Service
 
     void Awake()
     {
-        var uiDocument = GetComponent<UIDocument>();
-        screens = uiDocument.rootVisualElement.Query(className: "screen").ToList();
+        var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
+        screens = rootVisualElement.Query(className: "screen").ToList();
         ShowScreen(defaultScreenName);
+
+        // General button events
+        rootVisualElement.Q<Button>("join-back").clicked += () => { ShowScreen("title"); };
+        rootVisualElement.Q<Button>("title-buttons-join").clicked += () => { ShowScreen("join"); };
+        rootVisualElement.Q<Button>("title-buttons-create").clicked += () =>
+        {
+            Debug.Log("TODO: Generate map with no code");
+            ShowScreen("game");
+        };
+
+        // Join game events
+        var codeError = rootVisualElement.Q<Label>("join-inputs-error");
+        var codeInput = rootVisualElement.Q<TextField>("join-inputs-code");
+        codeInput.maxLength = 4;
+        rootVisualElement.Q<Button>("join-inputs-button").clicked += () =>
+        {
+            // Reset error text
+            codeError.text = String.Empty;
+
+            var code = codeInput.value;
+
+            if (code.Length == codeInput.maxLength)
+            {
+                Debug.Log("TODO: Generate map with code: " + code);
+                ShowScreen("game");
+            }
+            else
+            {
+                codeError.text = "Please enter a 4-digit game code";
+            }
+        };
     }
 
     public void ShowScreen(string screenName)
