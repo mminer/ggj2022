3ae8d39d9844861cf9f5352a36c6fb645571e421
Matthew Miner <matthew.miner@unity3d.com>
2022-01-30 14:35:15 -0800

    Fix monster sprites randomly changing
    Fixes #34.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Item
 {
+    public readonly int id;
     public readonly ItemType itemType;
     public Vector3Int movementDirection;
     public readonly PlayerType playerVisibility;
 
     public Item(ItemType itemType, PlayerType playerVisibility, Vector3Int movementDirection = default)
     {
+        this.id = Random.Range(0, int.MaxValue);
         this.itemType = itemType;
         this.movementDirection = movementDirection;
         this.playerVisibility = playerVisibility;
